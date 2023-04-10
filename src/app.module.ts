5df633c427836cdab74a235bca98d0cb4d939496
Patricia <kovach.patri@gmail.com>
2023-04-10 16:39:46 +0200

    add user controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DepartmentModule } from './department/module/department.module';
+import { UserService } from './user/service/user.service';
+import { UserController } from './user/controller/user.controller';
 
 @Module({
   imports: [DepartmentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
