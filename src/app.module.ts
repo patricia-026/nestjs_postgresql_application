47d4450ac54ca9515b4283d5b1c7df42afbad9ae
Patricia <kovach.patri@gmail.com>
2023-04-10 16:27:22 +0200

    add department service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DepartmentModule } from './department/department.module';
+import { DepartmentService } from './department/services/department.service';
+import { DepartmentController } from './department/controller/department.controller';
 
 @Module({
   imports: [DepartmentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, DepartmentController],
+  providers: [AppService, DepartmentService],
 })
 export class AppModule {}
--- src/department/module/department.module.ts
+++ src/department/module/department.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { Department } from '../models/department.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepartmentController } from '../controller/department.controller';
+import { DepartmentService } from '../service/department.service';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Department])
-    ]
+    ],
+    controllers: [DepartmentController],
+    providers: [DepartmentService]
 })
 export class DepartmentModule {}
